47d244493c2dd3cd74ea3c8051a99fd551817025
jhuva <jdzambranoh@unac.edu.pe>
2020-11-16 18:32:32 -0500

    Cambiando el nombre de login.css a styles.css y cambiando el link en login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&family=Righteous&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Infodata | Login</title>
 </head>
 <body>
