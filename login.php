cf5c6ed21736ebe18ce07697699c488ab35d78fc
jhuva <jdzambranoh@unac.edu.pe>
2020-11-16 18:27:12 -0500

    He agregado los input para el formulario y he creado un css para login

--- login.php
+++ login.php
@@ -6,11 +6,22 @@
     <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&family=Righteous&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Infodata | Login</title>
 </head>
 <body>
     
-
+    <section class="login">
+        <div class="login__container">
+            <h2>Administrador</h2>
+            <form class="login__container--form">
+                <input class="input" type="text" placeholder="Usuario">
+                <input class="input" type="password" placeholder="Contraseña">
+                <button class="btn btn-primary">Ingresar</button>
+            </form>
+            
+        </div>
+    </section>
 
 
 
